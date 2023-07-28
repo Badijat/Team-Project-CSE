1d67ec595d7bfb4e82866e3173e11a2ecc076c59
NathanLCleary <117375637+NathanLCleary@users.noreply.github.com>
2023-07-28 12:20:34 +0100

    Added classes to deal with Registering users
    Errors present - /Registration.xhtml @70,77 value="#{UsersTable.name}":
    Target Unreachable, identifier [UsersTable] resolved to null

--- src/main/java/ie/tus/User.java
+++ src/main/java/ie/tus/User.java
@@ -1,23 +1,31 @@
 package ie.tus;
 
-public class User {
-
-	private String username;
-	private String password;
-	private String email; 
+public class User {	
 	
-	public User(String username, String password, String email) {
-		this.username = username;
+	// user object to hold the users information
+
+	private String name, emailAddress, password;
+
+	public User(String name, String password, String email) {
+		this.name = name;
+		this.emailAddress = email;
 		this.password = password;
-		this.email = email;
+	}
+	
+	public String getname() {
+		return name;
 	}
 
-	public String getUsername() {
-		return username;
+	public void setname(String name) {
+		this.name = name;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
 	}
 
 	public String getPassword() {
@@ -28,11 +36,21 @@ public class User {
 		this.password = password;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getObscuredPassword() {
+		return (firstLetter(password) + "..." + lastLetter(password));
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public String register2() {
+		return ("show-registration");
 	}
+
+	private String firstLetter(String s) {
+		return (s.substring(0, 1));
+	}
+
+	private String lastLetter(String s) {
+		int length = s.length();
+		return (s.substring(length - 1, length));
+	}
+
 }
---
+++ src/main/java/ie/tus/UsersTable.java
@@ -0,0 +1,73 @@
+package ie.tus;
+
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="usersTable")
+@SessionScoped
+public class UsersTable {
+	
+		private ArrayList<User> users;
+		private String name, email, password;
+		
+		@PostConstruct
+		public void init() {
+			System.out.println("Datatable init");
+			users = new ArrayList<User>();
+			User firstName = new User("William Dupont", "Pass0rd", "william@gmail.com");
+			users.add(firstName);
+			
+			User secondName = new User("Harry Styles" , "NotHarryStyles", "harry@hotmail.com");
+			users.add(secondName);
+			
+			User thirdName = new User ("Jon BonJovi", "jon@hotmail.com", "jon@gmail.com");
+			users.add (thirdName);
+		}
+		
+		// get the entire list of users 
+		public ArrayList<User> getNames() {
+			System.out.println("Get names");
+			return users;
+		}
+		
+		
+		// calling this method creates a new user in the database
+		public String newUser() {
+			final User user = new User(this.name, this.password, this.email);
+			users.add(user);
+			this.name = null;
+			this.password  = null;
+			this.email = null;
+			System.out.println(users);
+			return null;
+		}
+		
+		// put the information from the form into these set methods and then call newUser to create a new user
+		public void setname(String name) {
+			this.name = name;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		
+		// these methods may not be needed
+		public String getPassword() {
+			return password;
+		}
+
+		public String getEmailAddress() {
+			return email;
+		}
+		public String getname() {
+			return name;
+		}
+	}
+
