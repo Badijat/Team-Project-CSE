f956a26aa0a910d8beed0968a933cb76dab39f05
Adijat Buraimoh <A00290545@student.ait.ie>
2023-07-30 12:41:49 +0000

    Create an xhtml and java page for Room availability

---
+++ src/main/java/ie/tus/AvailabilityBean.java
@@ -0,0 +1,78 @@
+package ie.tus;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@ManagedBean
+@ViewScoped
+public class AvailabilityBean implements Serializable {
+
+    private Date checkInDate;
+    private Date checkOutDate;
+    private int numGuests;
+    private String roomType;
+    private List<Room> availableRooms;
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public int getNumGuests() {
+        return numGuests;
+    }
+
+    public void setNumGuests(int numGuests) {
+        this.numGuests = numGuests;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public List<Room> getAvailableRooms() {
+        return availableRooms;
+    }
+
+    public void checkAvailability() {
+
+        availableRooms = new ArrayList<>();
+        availableRooms.add(new Room("Standard Room", 100.0, true));
+        availableRooms.add(new Room("Deluxe Room", 150.0, true));
+        availableRooms.add(new Room("Suite", 250.0, false));
+    }
+
+    // Inner class to represent a hotel room
+    public static class Room {
+        private String roomType;
+        private double price;
+        private boolean available;
+
+        public Room(String roomType, double price, boolean available) {
+            this.roomType = roomType;
+            this.price = price;
+            this.available = available;
+        }
+
+
+    }
+}
